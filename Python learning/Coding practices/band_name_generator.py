6d3d01bab4b97923572b891af0fce3fd063b3f8e
Carlos Bracho <brachocarlos152@gmail.com>
2024-05-06 16:17:11 -0400

    added band name generators coding challenge

---
+++ Python learning/Coding practices/band_name_generator.py
@@ -0,0 +1,8 @@
+# INSTRUCTIONS:
+# create a band name generator similar to this: https://appbrewery.github.io/python-day1-demo/
+
+print("Welcome to the Band Name Generatos.")
+city = input("What is the name of the city you grew up in? ")
+pet = input("What is your pet's name? ")
+
+band_name = print("You band name could be " + city + " " + pet)
